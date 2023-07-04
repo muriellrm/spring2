26b5d3a37677f6e1a81ef8a02fb83f95d79c4d40
Muriel Rampinelli Mazuchetti <muriel.rampinelli@gmail.com>
2023-07-04 16:06:53 -0300

    aula 20 validation

--- src/main/java/academy/devdojo/springboot2essentials/models/AnimePostRequest.java
+++ src/main/java/academy/devdojo/springboot2essentials/models/AnimePostRequest.java
@@ -2,7 +2,10 @@ package academy.devdojo.springboot2essentials.models;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class AnimePostRequest {
+    @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 }
