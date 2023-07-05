b2b1f6276ccbdf1a737ee784e9fb6ea9cac43f53
Muriel Rampinelli Mazuchetti <muriel.rampinelli@gmail.com>
2023-07-05 13:50:12 -0300

    aula 22 paginação

--- src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
@@ -7,6 +7,8 @@ import academy.devdojo.springboot2essentials.models.AnimePostRequest;
 import academy.devdojo.springboot2essentials.models.AnimePutRequest;
 import academy.devdojo.springboot2essentials.repository.AnimeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -18,8 +20,8 @@ public class AnimeService {
 
     private final AnimeRepository animeRepository;
 
-    public List<Anime> findAll() {
-        return animeRepository.findAll();
+    public Page<Anime> findAll(Pageable pageable) {
+        return animeRepository.findAll(pageable);
     }
 
     public List<Anime> findByName(String name) {
